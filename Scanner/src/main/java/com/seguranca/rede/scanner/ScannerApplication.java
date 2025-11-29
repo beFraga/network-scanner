e5675327367705039bb485e651b927603c454f93
Arthurclasen <162715667+Arthurclasen@users.noreply.github.com>
2025-11-29 10:20:27 -0300

    Update ScannerApplication.java

--- Scanner/src/main/java/com/seguranca/rede/scanner/ScannerApplication.java
+++ Scanner/src/main/java/com/seguranca/rede/scanner/ScannerApplication.java
@@ -11,15 +11,6 @@ public class ScannerApplication{
 
     public static void main(String[] args) {
         SpringApplication.run(ScannerApplication.class, args);
-        HttpComposite http = new HttpComposite("GET", "/home", 90);
-
-        TcpLeaf tcp1 = new TcpLeaf("192.168.0.2", "192.168.0.10", 5000, 80, 50);
-        TcpLeaf tcp2 = new TcpLeaf("192.168.0.2", "192.168.0.10", 5001, 80, 60);
-
-        http.addComponent(tcp1);
-        http.addComponent(tcp2);
-
-        http.showInfo();
     }
 
 }
